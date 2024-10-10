4f897d419623d44863c75fc0bad1874e3c6e3d9a
danimohli <danimohlife@gmail.com>
2024-10-10 21:32:45 +0100

    floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Function floor for Annotation
+"""
+
+def floor(n: float) -> int:
+
+    """
+    floor:
+        - Argumen N as float
+        - Return N as int
+    """
+    return int(n)
