820119b21e623a31c33010fca9d69571adea418c
danimohli <danimohlife@gmail.com>
2024-10-10 23:17:41 +0100

    get_value

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,26 +1,19 @@
 #!/usr/bin/env python3
-'''
-Type-annotated function to safely retrieve a value from a dictionary.
-'''
+"""
+type-annotated function
+"""
 from typing import Any, Mapping, Union, TypeVar
+
 
 T = TypeVar('T')
 Res = Union[Any, T]
 Def = Union[T, None]
 
 
-def safely_get_value(dct: Mapping[Any, T], key: Any,
-                     default: Def = None) -> Res:
-    '''Retrieves a value from a dictionary using a given key.
-
-    Args:
-        dct (Mapping): The dictionary to retrieve the value from.
-        key (Any): The key to look for in the dictionary.
-        default (Def, optional): The value to return if the key is not found.
-
-    Returns:
-        Res: The value associated with the key if found, otherwise default
-    '''
+def safely_get_value(dct: Mapping, key: Any, default: Def = None) -> Res:
+    """
+    Retrieves a value from a dict
+    """
     if key in dct:
         return dct[key]
     else:
