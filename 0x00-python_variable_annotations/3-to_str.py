035d178241e8955d4d98d20dbad6bec56a4aff1a
danimohli <danimohlife@gmail.com>
2024-10-10 21:39:49 +0100

    float_to_str

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Function to_str
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    to_str:
+        - N as Argument of float
+        - Return string rep of N
+    """
+    return str(n)
