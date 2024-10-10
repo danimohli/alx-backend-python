f43e53700716be86e786ee3e56963497f5e93bc9
danimohli <danimohlife@gmail.com>
2024-10-10 15:00:03 +0100

    add annotations

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Add function with notation.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Add function:
+     - Argument a float
+     - Argument b float
+     - Return a add b float
+    """
+    return a + b
