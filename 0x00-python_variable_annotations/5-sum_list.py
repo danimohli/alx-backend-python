05912accc3911ab52f05cf760ed2316667ce51fe
danimohli <danimohlife@gmail.com>
2024-10-10 22:00:26 +0100

    sum list

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Complex annotation for list of float
+"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    sum_list:
+        - input_list as Argument of list of float
+        - Return sum of the list as float
+    """
+    return sum(input_list)
