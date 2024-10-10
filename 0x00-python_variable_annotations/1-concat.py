5174e535060e12a814eb85fb0173dbe176429e9e
danimohli <danimohlife@gmail.com>
2024-10-10 21:24:33 +0100

    str_concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Function Annotation to concat str
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+
+    """
+    concat:
+        - str1 first Argument of string
+        - str2 second Argument of string
+        - Return both Argument as concat str
+    """
+
+    return str1 + str2
