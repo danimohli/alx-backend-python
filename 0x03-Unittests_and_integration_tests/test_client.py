1c43f6e8fa4d9befb7d0ece4efe3036bfcefb1ca
danimohli <danimohlife@gmail.com>
2024-11-02 17:39:21 +0100

    client

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+Unit tests for GithubOrgClient class.
+"""
+
+import unittest
+from unittest.mock import patch
+from parameterized import parameterized
+from client import GithubOrgClient
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """
+    Test case for GithubOrgClient's org method.
+    """
+
+    @parameterized.expand([
+        ("google",),
+        ("abc",),
+    ])
+    @patch('client.get_json')
+    def test_org(self, org_name, mock_get_json):
+        """
+        Test that GithubOrgClient.org calls get_json with the correct URL.
+        """
+        mock_get_json.return_value = {"login": org_name}
+        client = GithubOrgClient(org_name)
+        result = client.org
+        mock_get_json.assert_called_once_with(f"https://api.git\
+                                              hub.com/orgs/{org_name}")
+
+        self.assertEqual(result, {"login": org_name})
+
+
+if __name__ == "__main__":
+    unittest.main()
