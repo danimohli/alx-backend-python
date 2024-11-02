d52073dee4a8d2b6c586b93b04d5c5e301b0017a
danimohli <danimohlife@gmail.com>
2024-11-02 18:04:59 +0100

    test client

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -11,22 +11,37 @@ from client import GithubOrgClient
 
 class TestGithubOrgClient(unittest.TestCase):
     """
-    Test case for GithubOrgClient's org method.
+    Test case for GithubOrgClient's methods.
     """
+
     @parameterized.expand([
         ("google",),
         ("abc",),
     ])
     @patch('client.get_json')
-    def test_org(self, og, mock_json):
+    def test_org(self, org_name, mock_get_json):
         """
         Test that GithubOrgClient.org calls get_json with the correct URL.
         """
-        mock_json.return_value = {"login": og}
-        client = GithubOrgClient(og)
+        mock_get_json.return_value = {"login": org_name}
+        client = GithubOrgClient(org_name)
+
         result = client.org
-        mock_json.assert_called_once_with(f"https://api.github.com/orgs/{og}")
-        self.assertEqual(result, {"login": og})
+        mock_get_json.assert_called_once_with(f"https://api.github.com/\
+                                              orgs/{org_name}")
+        self.assertEqual(result, {"login": org_name})
+
+    @patch('client.GithubOrgClient.org',
+           new_callable=unittest.mock.PropertyMock)
+    def test_public_repos_url(self, mock_org):
+        """
+        Test that GithubOrgClient._public_repos_url returns the expected URL.
+        """
+        mock_org.return_value = {"repos_url":
+                                 "https://api.github.com/orgs/test_org/repos"}
+        client = GithubOrgClient("test_org")
+        self.assertEqual(client._public_repos_url,
+                         "https://api.github.com/orgs/test_org/repos")
 
 
 if __name__ == "__main__":
